070fc2dd0f64f26f88e6ff3a41c3e8f611f90214
jackiebailz <jackiebailz@protonmail.com>
2018-04-25 00:19:52 -0400

    Initial commit

---
+++ .config/lock/lock.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+scrot /tmp/screen.png
+convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
+picture=(/home/$USER/.config/lock/overlay.png)
+N=1
+
+for index in `shuf --input-range=0-$(( ${#picture[*]} - 1 )) | head -${N}`
+    do
+    figura=${picture[$index]} 
+done
+
+[[ -f $figura ]] && convert /tmp/screen.png $picture -gravity center -composite     -matte /tmp/screen.png
+i3lock -u -i /tmp/screen.png
